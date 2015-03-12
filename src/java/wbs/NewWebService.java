15aba9fa58af65136cd927058c48cdbe8796eed0
Sanat Rohatgi <rohatgisanat@gmail.com>
2015-03-12 17:32:50 +0530

    a
    a

---
+++ src/java/wbs/NewWebService.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wbs;
+
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+
+/**
+ *
+ * @author user
+ */
+@WebService(serviceName = "NewWebService")
+public class NewWebService {
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "add")
+    public int add(@WebParam(name = "i") int i, @WebParam(name = "j") int j) {
+        //TODO write your implementation code here:
+        return 0;
+    }
+
+    /**
+     * This is a sample web service operation
+     */
+    
+}
